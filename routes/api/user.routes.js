4c9d56336db949933e53a69e946be8d3f55609cf
Pamela Vasquez <pamelavasquez94@gmail.com>
2020-04-04 12:11:56 -0400

    set-up schemas for users and mood entry using mongodb

--- routes/api/user.routes.js
+++ routes/api/user.routes.js
@@ -1,5 +1,3 @@
 const router = require("express").Router();
 
-router.use("/", require("./user.routes"));
-
 module.exports = router;
--- server.js
+++ server.js
@@ -2,16 +2,66 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const morgan = require("morgan");
 const pages = require("./routes/pages");
+const mongoose = require("mongoose");
+const Users = require("./models/users.models");
+const CheckIn = require("./models/checkin.models");
 
 /* constants */
 const PORT = process.env.PORT || 3001;
+
+mongoose.connect("mongodb://localhost/bloom", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const app = express();
 
 /* set-up middlewares */
 app.use(morgan("dev")); // logging
-app.use(bodyparser.json()); // parsing json data in request body
+app.use(express.json());
 app.use("/", pages);
+
+app.get("/api/ping", (req, res) => res.send("pong"));
+
+app
+  .route("/api/users")
+  .get(async (req, res) => {
+    const users = await Users.find();
+    res.json(users);
+  })
+  .post(async (req, res) => {
+    const result = await Users.create(req.body);
+    res.json(result);
+  });
+
+app
+  .route("/api/users/:id/moods")
+  .get(async (req, res) => {
+    const user = await Users.findById(req.params.id).populate("moods");
+    res.json(user.moods);
+  })
+  .post(async (req, res) => {
+    try {
+      const mood = await CheckIn.create(req.body);
+      const results = await Users.findByIdAndUpdate(req.params.id, {
+        $push: { moods: mood._id }
+      });
+      res.json(results);
+    } catch (err) {
+      res.json(err);
+    }
+  });
+
+app
+  .route("/api/moods")
+  .get(async (req, res) => {
+    const moods = await CheckIn.find();
+    res.json(moods);
+  })
+  .post(async (req, res) => {
+    const result = await CheckIn.create(req.body);
+    res.json(result);
+  });
 
 /* run our app */
 app.listen(PORT, () => {
