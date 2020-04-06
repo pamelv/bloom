9a429bb980a06e39fa68a067821c7ff8f9b35400
Chloe <55222885+chloec222@users.noreply.github.com>
2020-04-06 11:11:47 -0400

    added recipe in client & fixed the server to run on react server & express server simultaneously, installed npm concurrently (#8)
    Co-authored-by: pamelv <53658724+pamelv@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Router, Route } from "react-router-dom";
 import history from "./history";
 import Signup from "./pages/Signup";
 import LogIn from "./pages/Login";
+import Recipe from "./pages/Recipe";
 import Home from "./pages/Home";
 import UserProfile from "./pages/UserProfile";
 
@@ -16,7 +17,7 @@ function App() {
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/login" component={LogIn} />
         <Route exact path="/profile" component={UserProfile} />
-
+        <Route exact path="/recipe" component={Recipe} />
       </div>
     </Router>
   );
