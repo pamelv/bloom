fb3390f3b3f30b27604908403b200ac1b5304161
Moon Jo <urwisdom77@gmail.com>
2020-04-09 12:36:06 -0400

    feat: Adding server side api calls from spotify and listenNote third party api

--- routes/pages/index.js
+++ routes/pages/index.js
@@ -2,4 +2,6 @@ const router = require("express").Router();
 
 router.use("/api", require("./recipe.routes"));
 router.use("/api", require("./poem.routes"));
+router.use("/api", require("./playlist.routes"));
+router.use("/api", require("./motivation.routes"));
 module.exports = router;
---
+++ routes/pages/playlist.routes.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+var Spotify = require("node-spotify-api");
+require("dotenv").config();
+
+var spotify = new Spotify({
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET,
+});
+
+router.get("/playlist", (req, res) => {
+  console.log("hello");
+  spotify
+    .request(
+      "https://api.spotify.com/v1/search?q=%22mood%22&type=playlist&limit=5"
+    )
+    .then(function (data) {
+      res.json(data.playlists.items);
+    })
+    .catch(function (err) {
+      console.error("Error occurred: " + err);
+    });
+});
+
+module.exports = router;
