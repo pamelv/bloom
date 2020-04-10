a9850705fd848d4e636b7248ec977f2d0d337d2e
Moon Jo <urwisdom77@gmail.com>
2020-04-09 21:23:28 -0400

    chore: resolving merge problem

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,5 @@
 import React from "react";
 import LogInForm from "../components/LoginForm";
-<<<<<<< HEAD
 
 export default function LogIn() {
   return (
@@ -12,13 +11,3 @@ export default function LogIn() {
     </div>
   );
 }
-=======
- 
-const LogIn = props => (
-  <div>
-    <LogInForm />
-  </div>
-);
-
-export default LogIn;
->>>>>>> 5340b5c9de82593d2bc8e5bda3693493598b9e95
