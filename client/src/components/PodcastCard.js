154746c2df9050ad4737f1aa53a85aecb60272a0
Moon Jo <urwisdom77@gmail.com>
2020-04-16 21:01:02 -0400

    style: style the podcast page

--- client/src/components/PodcastCard.js
+++ client/src/components/PodcastCard.js
@@ -1,9 +1,100 @@
 import React from "react";
+import cx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Button from "@material-ui/core/Button";
+import BookmarksTwoToneIcon from "@material-ui/icons/BookmarksTwoTone";
+import TextInfoContent from "@mui-treasury/components/content/textInfo";
+import { useFourThreeCardMediaStyles } from "@mui-treasury/styles/cardMedia/fourThree";
+import { useBlogTextInfoContentStyles } from "@mui-treasury/styles/textInfoContent/blog";
+import { useLightTopShadowStyles } from "@mui-treasury/styles/shadow/lightTop";
+
+const StyledButton = withStyles({
+  root: {
+    minWidth: "0px",
+  },
+  label: {
+    textTransform: "capitalize",
+  },
+})(Button);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: "170px",
+    borderRadius: 12,
+    margin: " 0 0 1em",
+    breakInside: "avoid",
+  },
+  media: {
+    borderRadius: 6,
+  },
+  content: {
+    padding: "10px",
+    paddingBottom: "0px !important",
+  },
+  "content:last-child": {
+    paddingBottom: "0px",
+  },
+  textContent: {
+    marginBottom: 0,
+    lineHeight: 1,
+  },
+}));
 
 export default function PodcastCard(props) {
+  const styles = useStyles();
+  const mediaStyles = useFourThreeCardMediaStyles();
+  const textCardContentStyles = useBlogTextInfoContentStyles();
+  const shadowStyles = useLightTopShadowStyles({ inactive: true });
   return (
-    <div className="mb-5">
-      <div
+    <div>
+      <Card className={cx(styles.root, shadowStyles.root)} key={props.id}>
+        <CardMedia
+          className={cx(styles.media, mediaStyles.root)}
+          image={props.image}
+        />
+        <CardContent className={styles.content}>
+          <TextInfoContent
+            classes={textCardContentStyles}
+            style={styles.textContent}
+            overline={`Total Time: ${props.audio_length_sec}`}
+            heading={props.title_original}
+            body={props.podcast_title_original}
+          />
+          <div
+            style={{ float: "right", minWidth: "150px", marginBottom: "10px" }}
+          >
+            <ButtonGroup
+              size="small"
+              style={{ color: "#9CCED5", marginRight: "5px" }}
+              aria-label="outlined secondary button group"
+            >
+              <Button
+                href={props.audio}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Listen
+              </Button>
+            </ButtonGroup>
+
+            <StyledButton
+              type="submit"
+              className="m-auto"
+              onClick={() => {
+                props.handleFormSave(props);
+              }}
+            >
+              <BookmarksTwoToneIcon style={{ color: "#C87B94" }} />
+            </StyledButton>
+          </div>
+        </CardContent>
+      </Card>
+
+      {/* <div
         key={props.id}
         className="card text-center m-auto shadow-lg p-3 mb-5 bg-white rounded-lg"
         style={{ width: "30rem" }}
@@ -26,7 +117,7 @@ export default function PodcastCard(props) {
             </button>
             {/* <a href={props.link} className="btn btn-primary"
                   style={{ marginLeft: "1rem" }}>BOOKMARK</a> */}
-            <button
+      {/* <button
               type="submit"
               className="mx-1"
               onClick={() => {
@@ -36,8 +127,7 @@ export default function PodcastCard(props) {
               BOOKMARK
             </button>
           </div>
-        </div>
-      </div>
+        </div> */}
     </div>
   );
 }
--- client/src/pages/Podcast.js
+++ client/src/pages/Podcast.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import API from "../utils/Podcast.api";
 import PodcastCard from "../components/PodcastCard";
+import Navbar from "../components/Navbar";
 import history from "../history";
 
 export default class Podcast extends Component {
@@ -66,28 +67,32 @@ export default class Podcast extends Component {
 
   render() {
     return (
-      <div>
-        <div>
-          <h5 className="text-center my-5">
-            DAILY PODCASTS CURATED JUST FOR YOU
-          </h5>
+      <div style={{ width: "100%", boxSizing: "border-box" }}>
+        <Navbar title="Podcast" />
+        <div
+          style={{
+            columnCount: "2",
+            columnGap: "1em",
+            padding: "0.7em",
+            height: "100%",
+          }}
+        >
+          {this.state.podcasts.map((podcast) => (
+            <div className="s12" value="mood" key={podcast.id}>
+              <PodcastCard
+                id={podcast.id}
+                title_original={podcast.title_original}
+                image={podcast.image}
+                podcast_title_original={podcast.podcast_title_original}
+                audio_length_sec={podcast.audio_length_sec}
+                audio={podcast.audio}
+                // link="#"
+
+                handleFormSave={this.handleFormSave}
+              />
+            </div>
+          ))}
         </div>
-
-        {this.state.podcasts.map((podcast) => (
-          <div className="col s12" value="mood" key={podcast.id}>
-            <PodcastCard
-              id={podcast.id}
-              title_original={podcast.title_original}
-              image={podcast.image}
-              podcast_title_original={podcast.podcast_title_original}
-              audio_length_sec={podcast.audio_length_sec}
-              audio={podcast.audio}
-              // link="#"
-
-              handleFormSave={this.handleFormSave}
-            />
-          </div>
-        ))}
       </div>
     );
   }
