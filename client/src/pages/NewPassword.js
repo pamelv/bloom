4fe0fa0d14834ece25a58c86b39b12c33a4a2a42
Moon Jo <urwisdom77@gmail.com>
2020-04-17 16:55:15 -0400

    style: style out the newpassword page

--- client/src/pages/NewPassword.js
+++ client/src/pages/NewPassword.js
@@ -1,10 +1,37 @@
 import React from "react";
 import NewPw from "../components/NewPWform";
+import pink from "../img/bloom_pink.gif";
+import blue from "../img/blue.gif";
+import orange from "../img/bloom_orange.gif";
 
 export default function NewPassword() {
   return (
     <div>
-      <NewPw />
+      <div
+        style={{
+          width: "60%",
+          position: "absolute",
+          top: "15%",
+          left: "50%",
+          transform: "translate(-50%,-50%)",
+        }}
+      >
+        <img style={{ width: "20vw" }} src={pink} alt="Bloom" />
+        <img style={{ width: "20vw" }} src={blue} alt="Bloom" />
+        <img style={{ width: "20vw" }} src={orange} alt="Bloom" />
+      </div>
+      <div
+        style={{
+          width: "70VW",
+          position: "absolute",
+          top: "55%",
+          left: "50%",
+          transform: "translate(-50%,-50%)",
+        }}
+      ></div>
+      <div>
+        <NewPw />
+      </div>
     </div>
   );
 }
